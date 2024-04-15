0e0a44b01eca57497356b03611c50abcaaf8ff8f
gibin19 <149844597+gibin19@users.noreply.github.com>
2024-04-15 19:43:13 -0400

    Update db.php

--- cssfinalproject/IMG/phpfianalproject/db.php
+++ cssfinalproject/IMG/phpfianalproject/db.php
@@ -1,14 +1,1 @@
-<?php
-$servername = "localhost"; // Assuming your database server is running locally
-$username = "your_username"; // Replace with your actual database username
-$password = "your_password"; // Replace with your actual database password
-$dbname = "your_database"; // Replace with your actual database name
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-?>
